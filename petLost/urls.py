d1f0d2a341523568d52819b887d6e7c2c1c11248
Guilherme Luis <guile.hm@hotmail.com>
2019-03-03 18:33:26 -0300

    changes to staticfiles

--- petLost/urls.py
+++ petLost/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -25,4 +26,7 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    ] + urlpatterns + static(
+        prefix=settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
