43a79a9a7fc22af0f0daec4304db36ffc8f516fa
Guilherme Luis <guile.hm@hotmail.com>
2019-03-03 17:25:31 -0300

    improve pet model and create validations

--- pet/models.py
+++ pet/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
 
@@ -34,13 +35,26 @@ class Pet(models.Model):
         (STATUS_FOUND, 'Found'),
     )
 
+    SEX_MALE = 'male'
+    SEX_FEMALE = 'female'
+    SEX_NOT_IDENTIFIED = 'not_identified'
+    SEX_CHOICES = (
+        (SEX_MALE, 'Male'),
+        (SEX_FEMALE, 'Female'),
+        (SEX_NOT_IDENTIFIED, 'Not Identified'),
+    )
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, null=True, blank=True)
+    sex = models.CharField(max_length=32, choices=SEX_CHOICES)
     kind = models.CharField(max_length=128, choices=TYPE_CHOICES, default=TYPE_DOG)
     slug = models.SlugField(db_index=True, unique=True)
     breed = models.ForeignKey('pet.Breed', on_delete=models.CASCADE, null=True)
     status = models.CharField(max_length=128, choices=STATUS_CHOICES, default=STATUS_LOST)
-    lost_date = models.DateField(default=timezone.now)
+    lost_date = models.DateField(db_index=True, null=True, blank=True)
+    found_date = models.DateField(db_index=True, null=True, blank=True)
+    picture = models.ForeignKey('pet.Picture', null=True, blank=True, on_delete=models.SET_NULL)
+    pictures = models.ManyToManyField('pet.Picture', related_name='pets', blank=True)
     description = models.TextField(null=True, blank=True)
 
     date_added = models.DateTimeField(auto_now_add=True)
@@ -48,6 +62,16 @@ class Pet(models.Model):
 
     def __str__(self):
         return f'{self.kind} #{str(self.id)[:8]} ({self.name})'
+
+    def clean(self):
+        if self.name and not self.lost_date:
+            self.lost_date = timezone.now()
+
+        if not any([self.lost_date, self.found_date]):
+            raise ValidationError('Lost Date or Found Date must be filled')
+
+        if self.lost_date and not self.name:
+            raise ValidationError('Please fill the name of your pet')
 
 
 class Picture(models.Model):
