c96d2b7d1ebd4715fcc2079deb02aa94626b03ed
Guilherme <guile.hm@hotmail.com>
2019-03-04 01:57:12 -0300

    fix bug

--- petLost/settings.py
+++ petLost/settings.py
@@ -14,10 +14,6 @@ import os
 
 import dj_database_url
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -118,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
@@ -139,4 +135,3 @@ STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 ALLOWED_HOSTS = ['gui-pets.herokuapp.com']
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
