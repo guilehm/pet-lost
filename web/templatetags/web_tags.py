22325ac9ad09c4beb1276414f4ee985d9bd7c050
Guilherme Luis <guile.hm@hotmail.com>
2019-03-04 00:23:58 -0300

    create custom tags

---
+++ web/templatetags/web_tags.py
@@ -0,0 +1,21 @@
+from django import template
+
+from web.models import Banner
+
+register = template.Library()
+
+translation_dict = {
+    'Male': 'Macho',
+    'Female': 'Fêmea',
+}
+
+
+@register.filter(name='translation')
+def translation(value):
+    return translation_dict.get(str(value), '')
+
+
+@register.inclusion_tag('web/tags/home_slider.html')
+def banners():
+    active_banners = Banner.objects.filter(active=True)
+    return {'banners': active_banners}
