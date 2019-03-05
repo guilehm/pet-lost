3d6a377291a17bdfac666a56ed38334f2a147d15
Guilherme Luis <guile.hm@hotmail.com>
2019-03-05 19:15:24 -0300

    change scope

--- petLost/settings.py
+++ petLost/settings.py
@@ -137,7 +137,7 @@ SOCIALACCOUNT_PROVIDERS = {
     'facebook':
         {
             'METHOD': 'oauth2',
-            'SCOPE': ['email', 'public_profile', 'user_friends'],
+            'SCOPE': ['email', 'public_profile'],
             'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
             'FIELDS': [
                 'id',
