cc989acb6c3978a97f2b25dfc7e985c0d38163f0
Guilherme Luis <guile.hm@hotmail.com>
2019-03-05 14:04:44 -0300

    install django alluth and setup facebook

--- petLost/settings_dev.py
+++ petLost/settings_dev.py
@@ -27,3 +27,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
