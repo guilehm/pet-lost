87abd81c53c8b71cc55d89202f0bed93b3eadac7
Guilherme Luis <guile.hm@hotmail.com>
2019-03-05 10:59:31 -0300

    minor improvements

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import ugettext_lazy as _
+
 from users.models import User
 
 
