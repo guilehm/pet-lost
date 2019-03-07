b2e65fb9e0867f973e5099c6eb0da5cbc06995c9
Guilherme Luis <guile.hm@hotmail.com>
2019-03-06 23:30:58 -0300

    refactor pet model

--- pet/models.py
+++ pet/models.py
@@ -1,25 +1,24 @@
-import urllib
 import uuid
 
-from django.core.exceptions import ValidationError
 from django.db import models
-from django.utils import timezone
+
+from announcement.models import Announcement
 
 
 class PetQuerySet(models.QuerySet):
     def lost(self):
         return self.filter(
-            situation=Pet.SITUATION_LOST,
             rescued=False,
             announcements__active=True,
-        )
+            announcements__situation=Announcement.SITUATION_LOST,
+        ).distinct()
 
     def found(self):
         return self.filter(
-            situation=Pet.SITUATION_FOUND,
             rescued=False,
             announcements__active=True,
-        )
+            announcements__situation=Announcement.SITUATION_FOUND,
+        ).distinct()
 
     def rescued(self):
         return self.filter(rescued=True)
@@ -43,16 +42,9 @@ class Breed(models.Model):
 
 
 class Pet(models.Model):
-    TYPE_DOG = 'dog'
-    TYPE_CHOICES = (
-        (TYPE_DOG, 'Dog'),
-    )
-
-    SITUATION_LOST = 'lost'
-    SITUATION_FOUND = 'found'
-    SITUATION_CHOICES = (
-        (SITUATION_LOST, 'Lost'),
-        (SITUATION_FOUND, 'Found'),
+    KIND_DOG = 'dog'
+    KIND_CHOICES = (
+        (KIND_DOG, 'Dog'),
     )
 
     SEX_MALE = 'male'
@@ -67,24 +59,16 @@ class Pet(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128, null=True, blank=True)
     sex = models.CharField(max_length=32, choices=SEX_CHOICES)
-    kind = models.CharField(max_length=128, choices=TYPE_CHOICES, default=TYPE_DOG)
+    kind = models.CharField(max_length=128, choices=KIND_CHOICES, default=KIND_DOG)
     slug = models.SlugField(db_index=True, unique=True)
-    breed = models.ForeignKey('pet.Breed', related_name='pets', on_delete=models.CASCADE, null=True)
-    situation = models.CharField(max_length=128, choices=SITUATION_CHOICES)
-    lost_date = models.DateField(db_index=True, null=True, blank=True)
-    found_date = models.DateField(db_index=True, null=True, blank=True)
-    rescued = models.BooleanField(default=False)
-    rescued_date = models.DateField(null=True, blank=True)
+    breed = models.ForeignKey(
+        'pet.Breed', related_name='pets', on_delete=models.CASCADE, null=True
+    )
     picture = models.ForeignKey(
         'pet.Picture', null=True, blank=True, on_delete=models.SET_NULL
     )
     pictures = models.ManyToManyField('pet.Picture', related_name='pets', blank=True)
     description = models.TextField(null=True, blank=True)
-    last_seen_district = models.CharField(max_length=512)
-    last_seen_city = models.ForeignKey(
-        'location.City', related_name='pets', db_index=True, on_delete=models.CASCADE
-    )
-    last_seen_detail = models.CharField(max_length=512)
 
     date_added = models.DateTimeField(auto_now_add=True)
     date_changed = models.DateTimeField(auto_now=True)
@@ -93,67 +77,6 @@ class Pet(models.Model):
 
     def __str__(self):
         return f'{self.kind} #{str(self.id)[:8]} ({self.name})'
-
-    def clean(self):
-        if not any([self.lost_date, self.found_date]):
-            raise ValidationError('Lost Date or Found Date must be filled')
-
-        if self.lost_date and not self.name:
-            raise ValidationError('Please fill the name of your pet')
-
-        if not self.situation:
-            if self.found_date:
-                self.situation = self.SITUATION_FOUND
-            elif self.lost_date:
-                self.situation = self.SITUATION_LOST
-
-        if self.rescued and not self.rescued_date:
-            self.rescued_date = timezone.now()
-
-    def save(self, *args, **kwargs):
-        self.clean()
-        return super().save(*args, **kwargs)
-
-    @property
-    def share_description(self):
-        return 'PetLost - Ajude {name} encontrar sua família. El{sex} conta com a sua ajuda!'.format(
-            name=self.name or 'este cãozinho a',
-            sex='a' if self.sex == self.SEX_FEMALE else 'e',
-        )
-
-    @property
-    def share_description_encoded(self):
-        description = self.share_description
-        return urllib.parse.urlencode({'text': description})
-
-    @property
-    def lost_description(self):
-        lost_description = ''
-        if self.situation == self.SITUATION_LOST:
-            text = f'Eu desapareci no dia {self.lost_date.strftime("%d/%m/%Y")}.\n'
-            lost_description += text
-        elif self.situation == self.SITUATION_FOUND:
-            text = 'Fui encontrad{sex} no dia {date}.\n'.format(
-                sex='a' if self.sex == self.SEX_FEMALE else 'o',
-                date=self.found_date.strftime("%d/%m/%Y")
-            )
-            lost_description += text
-        text = f'Me viram por último em {self.last_seen_city.data}, {self.last_seen_detail}\n'
-        lost_description += text
-        if self.sex == self.SEX_NOT_IDENTIFIED:
-            text = 'Ainda não identificaram meu sexo.'
-        elif self.sex == self.SEX_MALE:
-            text = 'Sou um macho.'
-        elif self.sex == self.SEX_FEMALE:
-            text = 'Sou uma fêmea.'
-        lost_description += f'{text}\n'
-        if not self.rescued:
-            text = 'Ainda não encontrei minha família, conto com sua ajuda para encontrá-los.'
-        else:
-            text = f'Fui recuperado por minha família em ' \
-                f'{self.rescued_date.strftime("%d/%m/%Y")} graças à ajuda de pessoas como você.'
-        lost_description += text
-        return lost_description
 
 
 class Picture(models.Model):
