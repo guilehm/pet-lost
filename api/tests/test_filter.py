c9bc76e177bf7076b246f4f7e818c7147dd3d7e5
Guilherme Luis <guile.hm@hotmail.com>
2019-09-04 20:57:36 -0300

    create tests for filtering by breed

--- api/tests/test_filter.py
+++ api/tests/test_filter.py
@@ -5,30 +5,13 @@ from django.urls import reverse
 @pytest.mark.django_db
 class TestPetFilterSetBySex:
 
-    def make_result_for_pet(self, pet):
-        if pet:
-            return {
-                'id': str(pet.id),
-                'slug': pet.slug,
-                'name': pet.name,
-                'sex': pet.sex,
-                'description': pet.description,
-                'breed': pet.breed,
-                'kind': pet.kind,
-                'mainPicture': None,
-                'pictures': [],
-                'announcements': [],
-                'dateAdded': str(pet.date_added),
-                'dateChanged': str(pet.date_changed),
-            }
-
     @pytest.fixture
     def pet_filter_male_response(self, pet_male, pet_female):
         return {
             'count': 1,
             'next': None,
             'previous': None,
-            'results': [self.make_result_for_pet(pet_male)]
+            'results': [make_result_for_pet(pet_male)]
         }
 
     @pytest.fixture
@@ -37,7 +20,7 @@ class TestPetFilterSetBySex:
             'count': 1,
             'next': None,
             'previous': None,
-            'results': [self.make_result_for_pet(pet_female)]
+            'results': [make_result_for_pet(pet_female)]
         }
 
     @pytest.fixture
@@ -72,3 +55,63 @@ class TestPetFilterSetBySex:
         url = f'{reverse("pet-list")}?sex=wrong'
         response = client.get(url, secure=True)
         assert response.json() == pet_filter_wrong_sex_response
+
+
+@pytest.mark.django_db
+class TestPetFilterSetByBreed:
+
+    @pytest.fixture
+    def pet_filter_breed_pug_response(self, pet_pug, pet_boxer):
+        return {
+            'count': 1,
+            'next': None,
+            'previous': None,
+            'results': [make_result_for_pet(pet_pug)]
+        }
+
+    @pytest.fixture
+    def pet_filter_breed_wrong_name_response(self, pet_pug, pet_boxer, pet, pet_male, pet_female, breed):
+        return {
+            'breed': [
+                "Select a valid choice. That choice is not one of the available choices."
+            ]
+        }
+
+    def test_pet_filter_by_breed_pug(
+            self, client, pet_pug, pet_boxer, pet_filter_breed_pug_response,
+    ):
+        assert pet_pug.breed.name == 'Pug'
+        assert pet_boxer.breed.name == 'Boxer'
+
+        url = f'{reverse("pet-list")}?breed=Pug'
+        response = client.get(url, secure=True)
+        response = response.json()
+        assert response == pet_filter_breed_pug_response
+        assert response['results'][0]['breed'] == 'Pug'
+        assert len(response['results']) == 1
+
+    def test_pet_filter_by_breed_wrong(self, client, pet_pug, pet_boxer, pet_filter_breed_wrong_name_response):
+        assert pet_pug.breed.name == 'Pug'
+        assert pet_boxer.breed.name == 'Boxer'
+
+        url = f'{reverse("pet-list")}?breed=Wrong'
+        response = client.get(url, secure=True)
+        assert response.json() == pet_filter_breed_wrong_name_response
+
+
+def make_result_for_pet(pet):
+    if pet:
+        return {
+            'id': str(pet.id),
+            'slug': pet.slug,
+            'name': pet.name,
+            'sex': pet.sex,
+            'description': pet.description,
+            'breed': pet.breed.name,
+            'kind': pet.kind,
+            'mainPicture': None,
+            'pictures': [],
+            'announcements': [],
+            'dateAdded': str(pet.date_added),
+            'dateChanged': str(pet.date_changed),
+        }
